cdddbb29cb5a215bb7f45338ef676d31fa3d3d45
german.vereschak <gvereschak@gotrg.com>
2019-06-02 17:06:50 +0300

    initial commit

---
+++ minter-store.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Minter Store
+Plugin URI: http://minterthestore.online
+Description: This plugin can provide functions to work with woocomerce to simple integration minter custom asset.
+Version: 1.0
+Author: gerka23@protonmail.com
+Author URI: http://mntstore.ru
+License: GPL2
+*/
