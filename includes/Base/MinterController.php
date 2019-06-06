b52d40901b2a63d3f2eddebb1d589ec5064d5ad5
german.vereschak <gvereschak@gotrg.com>
2019-06-06 18:10:34 +0300

    Struct struct struct

---
+++ includes/Base/MinterController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devacc
+ * Date: 06/06/2019
+ * Time: 18:09
+ */
+
+namespace MinterStore\Base;
+
+
+class MinterController
+{
+    public function register(){
+
+    }
+
+}
---
+++ includes/Base/PaymentControl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devacc
+ * Date: 27/02/2019
+ * Time: 12:36
+ */
+
+namespace MinterStore\Base;
+
+
+class PaymentControl extends BaseController
+{
+
+    public function register(){
+
+    }
+
+}
