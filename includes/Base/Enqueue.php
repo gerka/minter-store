e3f376cf8491c91917056fcb9bf4f87805f700cc
German Vereschak <gerka23@protonmail.com>
2019-07-01 11:59:51 +0000

    enqueue template

--- includes/Base/Enqueue.php
+++ includes/Base/Enqueue.php
@@ -22,11 +22,46 @@ class Enqueue extends BaseController
     public function register(){
         add_action('admin_enqueue_scripts', [$this,'admin_enqueue']);
         add_action( 'template_redirect', [$this,'public_enqueue'] );
+        add_action( 'wp_enqueue_scripts', array( $this, 'payment_enqueue' ) );
+
     }
     public function admin_enqueue(){
         wp_enqueue_style(self::getPluginName().'-style',self::getPluginUrl().'assets/mystyle.css');
+        wp_enqueue_script(self::getPluginName().'-script-admin',self::getPluginUrl().'assets/minter-store-admin.js');
+
     }
     public function public_enqueue(){
 
+    }
+    public function payment_enqueue(){
+// We need custom JavaScript to obtain a token
+
+        // we need JavaScript to process a token only on cart/checkout pages, right?
+        if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
+            return;
+        }
+
+        // if our payment gateway is disabled, we do not have to enqueue JS too
+        if ( 'no' === $this->enabled ) {
+            return;
+        }
+
+        // no reason to enqueue JavaScript if API keys are not set
+        if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
+            return;
+        }
+
+        // do not work with card detailes without SSL unless your website is in a test mode
+        if ( ! $this->testmode && ! is_ssl() ) {
+            return;
+        }
+
+//        // and this is our custom JS in your plugin directory that works with token.js
+//        wp_register_script( 'woocommerce_minter-store', plugins_url( 'minter-store.js', __FILE__ ), array( 'jquery', 'minter-store_js' ) );
+//
+//        wp_localize_script( 'woocommerce_minter-store', 'minter-store_params', array(
+//        ) );
+//
+//        wp_enqueue_script( 'woocommerce_minter-store' );
     }
 }
